948156799e8c76786eb470ed6f24f60b7e6ff272
ProgrammingInBlood <eklavyasingh12065@gmail.com>
2021-08-28 20:24:32 +0530

    deploy

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  images: {
+    domains: [
+      "ecrm.tripazzo.in",
+      "api.bedsdivans.co.uk",
+      "localhost",
+      "admin.bedsdivans.co.uk",
+    ],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
